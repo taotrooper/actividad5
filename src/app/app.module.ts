e41c214d3608bcc28cb77250654119e37de2e725
Christel González Canales <kirisame@gmail.com>
2023-07-07 22:18:17 +0200

    Creación de componente blog
    Creado componente components/blog y cambiado el html del padre para mostrar el componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule

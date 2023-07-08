4a38a7d9177e96376f066b70deca26da6de0bb32
Christel González Canales <kirisame@gmail.com>
2023-07-09 00:02:12 +0200

    Hecha la feature de listado y parcialmente la de formulario
    Listo el css y html preliminar. Muestra las noticias en el array y las que han sido añadidas con el formulario correctamente. Añadido FormsModule y creado el formulario en una versión simple (hacer push sin verificación de datos). Sólo falta aplicar los controles de formulario, investigar cómo hacer la parte de la fecha, y hacer pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BlogComponent } from './components/blog/blog.component';
@@ -10,7 +11,8 @@ import { BlogComponent } from './components/blog/blog.component';
     BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

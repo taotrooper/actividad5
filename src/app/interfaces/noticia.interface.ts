1e97502b8e7dde14b9ce6c0cb7d1ad3c1645316e
Christel González Canales <kirisame@gmail.com>
2023-07-09 04:32:29 +0200

    Hecha la feature de formulario junto con sus validaciones

--- src/app/interfaces/noticia.interface.ts
+++ src/app/interfaces/noticia.interface.ts
@@ -2,5 +2,5 @@ export interface Noticia {
     titulo: string,
     imagen: string,
     texto: string,
-    fecha: string //cambiar a tipo Date más adelante
+    fecha: string // se debería cambiar a tipo Date en un futuro, pero mejor esperar a ver validaciones en Angular en clase antes
 }
